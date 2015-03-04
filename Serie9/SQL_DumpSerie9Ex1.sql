47d3a4ea33e30a71f7506f95ee0f865e785ff69e
Steve Visinand <visinandst@gmail.com>
2015-03-04 11:43:16 +0100

    Modification de la BDD pour le support de l'encodage MD5 du password

--- Serie9/SQL_DumpSerie9Ex1.sql
+++ Serie9/SQL_DumpSerie9Ex1.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le: Mer 04 Mars 2015 à 10:49
+-- Généré le: Mer 04 Mars 2015 à 11:42
 -- Version du serveur: 5.5.29
 -- Version de PHP: 5.4.10
 
@@ -22,15 +22,15 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `user` (
   `user_id` int(10) NOT NULL AUTO_INCREMENT,
-  `pseudo` varchar(25) NOT NULL,
-  `pass` varchar(25) NOT NULL,
+  `pseudo` varchar(20) NOT NULL,
+  `pass` varchar(32) NOT NULL,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `pseudo` (`pseudo`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Contenu de la table `user`
 --
 
 INSERT INTO `user` (`user_id`, `pseudo`, `pass`) VALUES
-(1, '1', 'toto');
+(5, 'toto', '81dc9bdb52d04dc20036dbd8313ed055');
