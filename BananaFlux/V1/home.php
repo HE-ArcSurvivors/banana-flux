7b394071dfbff487e06b12c6137d2ffce5ae7302
Steve Visinand <visinandst@gmail.com>
2015-04-19 16:28:08 +0200

    Affichage simple des dossiers de l'utilisateur

--- BananaFlux/V1/home.php
+++ BananaFlux/V1/home.php
@@ -25,14 +25,58 @@ WHERE `feed`.`feed_id` = `feed_folder`.`feed_id` AND `feed_folder`.`folder_id` =
 	
 	if(!$resource)
 	{
-		echo "Connection error: ".mysqli_connect_errno();
+		return "Connection error: ".mysqli_connect_errno();
 	}
 	else
 	{
-	   	$row = mysqli_fetch_array($resource);
-	   	
-	   	print_r($row);
+		$toShow = "";
+		
+		$curentFolder_id=null;
+		while ($record = mysqli_fetch_assoc ($resource))
+		{
+			if($record['folder_id']!=$curentFolder_id)
+			{
+				//end old folder
+				if($curentFolder_id != null)
+				{
+					$toShow.="</div>";
+				}
+				
+				$curentFolder_id =$record['folder_id'];
+				
+				//new folder
+				$toShow .='<div class="dossier">
+				   <div class="dossierHead">
+					   <p>'.$record['folder_name'].'</p>
+				   		<span class="control_elementLeftFlap">
+				   			<span class="open fa fa-plus-square-o"></span>
+				   			<span class="edit fa fa-pencil-square-o"></span>
+				   			<span class="suppr fa fa-times"></span>
+				   		</span>
+				   		<span class="iddossier_hidden">'.$record['folder_id'].'</span>
+				   </div>';
+				
+			}
+			
+			//print flux
+			
+			$toShow .='<div class="flux">
+			   		<p>'.$record['feed_title'].'</p>
+			   		<span class="control_elementLeftFlap">
+			   			<span class="edit fa fa-pencil-square-o"></span>
+			   			<span class="suppr fa fa-times"></span>
+			   		</span>
+			   		<span class="idflux_hidden">'.$record['feed_id'].'</span>
+			   		</div>';
+			 
+		}
+		
+		$toShow.="</div>"; //close the last folder
+		
+		return $toShow;
 	}
+	
+	
 
 
 }
@@ -78,6 +122,8 @@ WHERE `feed`.`feed_id` = `feed_folder`.`feed_id` AND `feed_folder`.`folder_id` =
 		   <div id="leftFlap_Content">
 			   <h1>Dossiers</h1>
 			   <h2>Personnels</h2>
+			   
+			   <!--
 			   <div class="dossier">
 				   <div class="dossierHead">
 					   <p>Un dossier</p>
@@ -108,10 +154,17 @@ WHERE `feed`.`feed_id` = `feed_folder`.`feed_id` AND `feed_folder`.`folder_id` =
 			   		</div>
 			   		
 			   </div>
+			   -->
+			   
+			   <?php
+			   		echo $dossiers;
+			   ?>
 			   
 			   <h2>Par défaut</h2>
 			   
 			   <h1>Flux</h1>
+			   
+			   <!--
 			   <div class="flux">
 			   		<p>20 Minutes</p>
 			   		<span class="control_elementLeftFlap">
@@ -128,6 +181,7 @@ WHERE `feed`.`feed_id` = `feed_folder`.`feed_id` AND `feed_folder`.`folder_id` =
 			   		</span>
 			   		<span class="idflux_hidden">2</span>
 			   </div>
+			   -->
 	
 		   </div>
 		   <div id="leftFlap_Bouton"><p>Ouvrir</p></div>
