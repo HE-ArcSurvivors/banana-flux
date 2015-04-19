405dc3f9e13a51cfbaefea23e46bbb73f772d289
Steve Visinand <visinandst@gmail.com>
2015-04-19 13:08:11 +0200

    Implémentation des classes Article et Feed et utilisation dans showArticles

--- BananaFlux/V1/article.php
+++ BananaFlux/V1/article.php
@@ -2,25 +2,72 @@
 
 class Article {
 
-    private $title;
-    private $description;
-    private $url;
-    private $img;
+    private $_title;
+    private $_description;
+    private $_url;
+    private $_img;
 
-    public function __construct()
+    public function __construct($title, $description, $url, $img)
     {
-	   
+	   $this->_title = $title;
+	   $this->_description = $description;
+	   $this->_url = $url;
+	   $this->_img = $img;
     }
    
+/*
     public function saveForLater()
     {
        return true;
     }
-    
-    public function printArticle()
+*/
+    private function cutText($string, $length)
+	{
+		//Hard cut ! amélioration possible
+		if(strlen($string)>$length)
+		{
+			$string = substr ($string, 0, $length).'...'; 
+		}
+		
+		return $string;
+	}
+	
+    public function getApercu()
     {
-       
+       	//Size of the strings
+		$lengthDescr = "50";
+		$lengthDescrSansImage = "1000";
+		$lengthTitle = "60";
+		
+		//Image ?
+		$showImg = "";
+		if(!empty($this->_img))
+		{
+			$showImg = '<div class="article_image"><img src="'.$this->_img.'" alt="'.$this->_title.'"></div>';
+			
+			$descr = $this->cutText($this->_description, $lengthDescr);
+		}
+		else
+		{
+			$descr = $this->cutText($this->_description, $lengthDescrSansImage);
+		}
+		
+		$descr.='[<a href="'.$this->_url.'" target="_blank">Lire la suite</a>]';
+		$title = $this->cutText($this->_title, $lengthTitle);
+		
+		//Return the constructed article
+		return '<div id="articles"><div class="article">
+	      		<div class="article_head">
+	      			<h1>'.$this->_title.'</h1>
+	      		</div>
+	      		'.$showImg.'
+	      		<div class="article_descritption">
+	      			<p>'.$descr.'</p>
+	      		</div>
+	      		</div></div>';
+
     }
+    
     
 }
 
--- BananaFlux/V1/showArticles.php
+++ BananaFlux/V1/showArticles.php
@@ -1,73 +1,12 @@
 <?php
 	require_once "header.php";
 	
+	require_once "feed.php";
+	
 	$nbShowed=@$_POST['nbshowed'];
 	$nbAdd=@$_POST['nbadd'];
 	$id_flux=@$_POST['idflux'];
 	$id_dossier=@$_POST['iddossier'];
-	
-	function cutText($string, $length)
-	{
-		//Hard cut ! amélioration possible
-		if(strlen($string)>$length)
-		{
-			$string = substr ($string, 0, $length).'...'; 
-		}
-		
-		return $string;
-	}
-	
-	function article($title, $descr, $url, $image="")
-	{
-		//Size of the strings
-		$lengthDescr = "50";
-		$lengthDescrSansImage = "1000";
-		$lengthTitle = "60";
-		
-		//Image ?
-		$showImg = "";
-		if(!empty($image))
-		{
-			$showImg = '<div class="article_image"><img src="'.$image.'" alt="'.$title.'"></div>';
-			
-			$descr = cutText($descr, $lengthDescr);
-		}
-		else
-		{
-			$descr = cutText($descr, $lengthDescrSansImage);
-		}
-		
-		$descr.='[<a href="'.$url.'" target="_blank">Lire la suite</a>]';
-		$title = cutText($title, $lengthTitle);
-		
-		//return the constructed article
-		return '<div id="articles"><div class="article">
-	      		<div class="article_head">
-	      			<h1>'.$title.'</h1>
-	      		</div>
-	      		'.$showImg.'
-	      		<div class="article_descritption">
-	      			<p>'.$descr.'</p>
-	      		</div>
-	      		</div></div>';
-	}
-	
-	function showArticlesRSS($urlRSS, $idfirst, $idlast)
-	{
-		$content = file_get_contents($urlRSS);
-		$listflux = new SimpleXmlElement($content);
-    
-		$i=0;
-		foreach($listflux->channel->item as $entry)
-		{   
-    		if($i<=$idlast+$idfirst && $i > $idfirst)
-    		{
-    			echo article($entry->title, $entry->description, $entry->link, $entry->image);
-    		}
-    		$i++;
-    	}
-
-	}
 	
 	//http://www.20min.ch/rss/rss.tmpl?type=rubrik&get=313&lang=ro
 	//http://www.jeuxvideo.com/rss/rss.xml
@@ -75,22 +14,8 @@
 	if(empty($id_dossier) && !empty($id_flux))
 	{
 		//afficher flux unique
-		
-		$sql = "SELECT `feed_url` FROM `feed` WHERE `feed_id`=".$id_flux;
-		$resource = mysqli_query($db, $sql);
-		
-		if(!$resource)
-		{
-			echo "Connection error: ".mysqli_connect_errno(); //TODO
-		}
-		else
-		{
-	   		$row = mysqli_fetch_array($resource);
-	   		$urlRSS = $row[0];
-	   		
-	   		showArticlesRSS($urlRSS, $nbShowed, $nbAdd);
-	   }
-
+		$feed = new feed($id_flux, $db);
+		echo $feed->getArticlesFeed($nbShowed, $nbShowed+$nbAdd);
 		
 	}
 	else if(!empty($id_dossier) && empty($id_flux))
